f7fed99f62337c29cc76d9409b835d7ee7dba790
tumpio <tumpio@sci.fi>
2016-10-29 12:05:37 +0300

    Support for new rule and actions format

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 const myOptionsManager = new OptionsManager();
 const tldStarPattern = /^(.+)\.\*$/;
 const redirectUrlPattern = /^https?:\/\/.+(https?)(:\/\/|%3A\/\/|%3A%2F%2F)(.+)$/;
+const requestListeners = [];
+
+function requestAction(action, redirectUrl) {
+    switch (action) {
+        case "filter":
+            return function (request) {
+                if (request.method != "GET" || request.tabId == -1) {
+                    return;
+                }
+                let redirectUrl = parseRedirectUrl(request.url);
+                if (redirectUrl.length < request.url.length) {
+                    chrome.tabs.update(request.tabId, {
+                        url: parseRedirectUrl(request.url)
+                    });
+                    return {
+                        cancel: true
+                    };
+                }
+            };
+        case "block":
+            return function (request) {
+                return {
+                    cancel: true
+                };
+            };
+        case "redirect":
+            return function (request) {
+                return {
+                    redirectUrl: redirectUrl
+                };
+            };
+    }
+}
 
 function redirectUrlReplacer(match, p1, p2, p3, offset, string) {
     if (p2[0] == "%") {
@@ -32,29 +65,14 @@ function filterUnwantedQueryParams(url) {
     return urlParts.join("?");
 }
 
-function redirect(request) {
-    if (request.method != "GET" || request.tabId == -1) {
-        return;
-    }
-    let redirectUrl = parseRedirectUrl(request.url);
-    if (redirectUrl.length < request.url.length) {
-        chrome.tabs.update(request.tabId, {url: parseRedirectUrl(request.url)});
-        return {
-            cancel: true
-        };
-    }
-}
-
-function resolveUrls(rules) {
+function resolveUrls(pattern) {
     let urls = [];
-    for (let rule of rules) {
-        if (tldStarPattern.test(rule.pattern.host)) {
-            for (let TLD of rule.pattern.topLevelDomains) {
-                urls.push(tldStarPatternRuleToUrl(rule.pattern, TLD));
-            }
-        } else {
-            urls.push(patternRuleToUrl(rule.pattern));
+    if (tldStarPattern.test(pattern.host)) {
+        for (let TLD of pattern.topLevelDomains) {
+            urls.push(tldStarPatternRuleToUrl(pattern, TLD));
         }
+    } else {
+        urls.push(patternRuleToUrl(pattern));
     }
     return urls;
 }
@@ -63,20 +81,37 @@ function patternRuleToUrl(pattern) {
     return pattern.scheme + "://" + (pattern.matchSubDomains ? "*." : "") + pattern.host + "/" + pattern.path;
 }
 
-
 function tldStarPatternRuleToUrl(pattern, TLD) {
-    return pattern.scheme + "://" + (pattern.matchSubDomains ? "*." : "") + pattern.host.replace(tldStarPattern, "$1." + TLD)  + "/" +  pattern.path;
+    return pattern.scheme + "://" + (pattern.matchSubDomains ? "*." : "") + pattern.host.replace(tldStarPattern, "$1." +
+        TLD) + "/" + pattern.path;
+}
+
+function removePreviousListeners() {
+    let listener;
+    while (listener = requestListeners.pop()) {
+        chrome.webRequest.onBeforeRequest.removeListener(listener);
+    }
+}
+
+function addListeners() {
+    let filter, listener;
+    for (let rule of myOptionsManager.options.rules) {
+        if (!rule.active) {
+            continue;
+        }
+        filter = {
+            urls: resolveUrls(rule.pattern),
+            types: rule.types
+        };
+        listener = new requestAction(rule.action, rule.redirectUrl);
+        chrome.webRequest.onBeforeRequest.addListener(listener, filter, ["blocking"]);
+        requestListeners.push(listener);
+    }
 }
 
 function init() {
-    chrome.webRequest.onBeforeRequest.removeListener(redirect);
-    chrome.webRequest.onBeforeRequest.addListener(redirect, {
-        urls: resolveUrls(myOptionsManager.options.rules),
-        types: [
-            "main_frame",
-            "sub_frame"
-        ]
-    }, ["blocking"]);
+    removePreviousListeners();
+    addListeners();
 }
 
 myOptionsManager.loadOptions(init);
