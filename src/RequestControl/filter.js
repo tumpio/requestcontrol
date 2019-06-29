c4d27868d483aa48838d8efaead0bdcc1dcb8086
tumpio <tumpio@post.com>
2019-06-29 20:01:16 +0300

    Refactor

--- src/RequestControl/filter.js
+++ src/RequestControl/filter.js
@@ -2,44 +2,50 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {ControlRule, FILTER_ACTION} from "./base.js";
-import {createRegexpPattern} from "./api.js";
-import {processRedirectRules} from "./redirect.js";
-import {DomainMatcher} from "./matchers.js";
-import {parseInlineUrl, trimQueryParameters, UrlParser} from "./url.js";
+import { ControlRule, FILTER_ACTION } from "./base.js";
+import { createRegexpPattern } from "./api.js";
+import { processRedirectRules } from "./redirect.js";
+import { DomainMatcher } from "./matchers.js";
+import { parseInlineUrl, trimQueryParameters, UrlParser } from "./url.js";
 
 export class FilterRule extends ControlRule {
-    constructor(uuid, paramsFilter, removeQueryString, skipInlineUrlFilter, skipOnSameDomain, matcher) {
+    constructor(uuid, paramsFilter, removeQueryString, skipInlineUrlParsing, skipOnSameDomain, matcher) {
         super(uuid, matcher);
         this.queryParamsPattern = (paramsFilter) ? createRegexpPattern(paramsFilter.values) : "";
-        this.invertQueryFilter = (paramsFilter) ? paramsFilter.invert : false;
+        this.invertQueryTrimming = (paramsFilter) ? paramsFilter.invert : false;
         this.removeQueryString = removeQueryString;
-        this.skipInlineUrlFilter = skipInlineUrlFilter;
+        this.skipInlineUrlParsing = skipInlineUrlParsing;
         this.skipOnSameDomain = skipOnSameDomain;
     }
 
     apply(url) {
-        let filteredUrl = url;
         // Trim unwanted query parameters before parsing inline url
-        if (!this.removeQueryString) {
-            filteredUrl = trimQueryParameters(url, this.queryParamsPattern, this.invertQueryFilter);
-        }
-        if (!this.skipInlineUrlFilter) {
-            let inlineUrl = parseInlineUrl(filteredUrl);
-            if (inlineUrl != null) {
-                if (this.skipOnSameDomain && DomainMatcher.testUrls(url, inlineUrl)) {
-                    return filteredUrl;
-                }
-                filteredUrl = inlineUrl;
-            }
-        }
+        let trimmedUrl = this.filterQueryParameters(url);
+        trimmedUrl = this.filterInlineUrl(trimmedUrl);
+        return this.filterQueryParameters(trimmedUrl);
+    }
+
+    filterQueryParameters(url) {
         if (this.removeQueryString) {
-            let parser = new UrlParser(filteredUrl);
+            let parser = new UrlParser(url);
             parser.search = "";
             return parser.href;
         }
-        return trimQueryParameters(filteredUrl, this.queryParamsPattern, this.invertQueryFilter);
+        return trimQueryParameters(url, this.queryParamsPattern, this.invertQueryTrimming);
     }
+
+    filterInlineUrl(url) {
+        if (this.skipInlineUrlParsing) {
+            return url;
+        }
+        let inlineUrl = parseInlineUrl(url);
+        if (inlineUrl == null 
+            || this.skipOnSameDomain && DomainMatcher.testUrls(url, inlineUrl)) {
+            return url;
+        }
+        return inlineUrl;
+    }
+
 }
 
 FilterRule.prototype.priority = -2;
