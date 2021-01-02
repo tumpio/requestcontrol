44c471dd05a197f8b7320952679b8a74c6df2f9f
tumpio <tumpio@post.com>
2021-01-02 09:41:23 +0200

    Update rule tester

--- src/options/rule-tester.js
+++ src/options/rule-tester.js
@@ -2,13 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { createMatchPatterns, createRule } from "../main/api.js";
+import { createRequestFilters } from "../main/api.js";
 import { RequestController, CompositeRule } from "../main/control.js";
 import { LoggedWhitelistRule, WhitelistRule } from "../main/rules/whitelist.js";
 import { BlockRule } from "../main/rules/block.js";
 import { RedirectRule } from "../main/rules/redirect.js";
 import { FilterRule } from "../main/rules/filter.js";
 import { SecureRule } from "../main/rules/secure.js";
+import { matchPatternToRegExp } from "../util/regexp.js";
 
 export function testRules(testUrl, rulePatterns) {
     try {
@@ -21,10 +22,12 @@ export function testRules(testUrl, rulePatterns) {
 
     try {
         for (const rulePattern of rulePatterns) {
-            const rule = createRule(rulePattern);
-            const matchPatterns = createMatchPatterns(rulePattern.pattern);
-            for (const matchPattern of matchPatterns) {
-                if (matchPatternToRegExp(matchPattern).test(request.url)) {
+            const filters = createRequestFilters(rulePattern);
+            for (const { rule, urls, matcher } of filters) {
+                if (
+                    urls.map(matchPatternToRegExp).some((pattern) => pattern.test(request.url)) &&
+                    matcher.test(request)
+                ) {
                     controller.mark(request, rule);
                     break;
                 }
@@ -55,7 +58,7 @@ function testRule(rule, testUrl) {
             try {
                 new URL(redirectUrl);
             } catch {
-                return browser.i18n.getMessage("invalid_target_url") + redirectUrl;
+                return browser.i18n.getMessage("invalid_target_url", redirectUrl);
             }
             if (redirectUrl === testUrl) {
                 return browser.i18n.getMessage("matched_no_change");
@@ -72,7 +75,7 @@ function testRule(rule, testUrl) {
             try {
                 new URL(redirectUrl);
             } catch {
-                return browser.i18n.getMessage("invalid_target_url") + redirectUrl;
+                return browser.i18n.getMessage("invalid_target_url", redirectUrl);
             }
             if (redirectUrl === testUrl) {
                 return browser.i18n.getMessage("matched_no_change");
@@ -83,68 +86,4 @@ function testRule(rule, testUrl) {
         default:
             break;
     }
-}
-
-/**
- * Transforms a valid match pattern into a regular expression
- * which matches all URLs included by that pattern.
- * Source: MDN
- * @param  {string}  pattern  The pattern to transform.
- * @return {RegExp}           The pattern's equivalent as a RegExp.
- * @throws {TypeError}        If the pattern is not a valid MatchPattern
- */
-function matchPatternToRegExp(pattern) {
-    if (pattern === "" || pattern === "<all_urls>") {
-        return /^(?:http|https):\/\//;
-    }
-
-    const schemeSegment = "(\\*|http|https)";
-    const hostSegment = "(\\*|(?:\\*\\.)?(?:[^/*]+))?";
-    const pathSegment = "(.*)";
-    const matchPatternRegExp = new RegExp(`^${schemeSegment}://${hostSegment}/${pathSegment}$`);
-    const regexpChars = /[$()+.?[\\\]^{|}]/g; // excluding "*"
-
-    const match = matchPatternRegExp.exec(pattern);
-    if (!match) {
-        throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
-    }
-
-    const [, scheme, , path] = match;
-    let host = match[2];
-    if (!host) {
-        throw new TypeError(`"${pattern}" does not have a valid host`);
-    }
-
-    let regex = "^";
-
-    if (scheme === "*") {
-        regex += "(http|https)";
-    } else {
-        regex += scheme;
-    }
-
-    regex += "://";
-
-    if (host && host === "*") {
-        regex += "[^/]+?";
-    } else if (host) {
-        if (host.match(/^\*\./)) {
-            regex += "[^/]*?";
-            host = host.substring(2);
-        }
-        regex += host.replace(regexpChars, "\\$&");
-    }
-
-    if (path) {
-        if (path === "*") {
-            regex += "(/.*)?";
-        } else if (path.charAt(0) !== "/") {
-            regex += "/";
-            regex += path.replace(regexpChars, "\\$&").replace(/\*/g, ".*?");
-            regex += "/?";
-        }
-    }
-
-    regex += "$";
-    return new RegExp(regex);
 }
--- src/util/regexp.js
+++ src/util/regexp.js
@@ -29,6 +29,70 @@ export function createRegexpPattern(values, insensitive = true, containing = fal
     return new RegExp(pattern.substring(1), flags);
 }
 
+/**
+ * Transforms a valid match pattern into a regular expression
+ * which matches all URLs included by that pattern.
+ * Source: MDN
+ * @param  {string}  pattern  The pattern to transform.
+ * @return {RegExp}           The pattern's equivalent as a RegExp.
+ * @throws {TypeError}        If the pattern is not a valid MatchPattern
+ */
+export function matchPatternToRegExp(pattern) {
+    if (pattern === "" || pattern === "<all_urls>") {
+        return /^(?:http|https):\/\//;
+    }
+
+    const schemeSegment = "(\\*|http|https)";
+    const hostSegment = "(\\*|(?:\\*\\.)?(?:[^/*]+))?";
+    const pathSegment = "(.*)";
+    const matchPatternRegExp = new RegExp(`^${schemeSegment}://${hostSegment}/${pathSegment}$`);
+    const regexpChars = /[$()+.?[\\\]^{|}]/g; // excluding "*"
+
+    const match = matchPatternRegExp.exec(pattern);
+    if (!match) {
+        throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
+    }
+
+    const [, scheme, , path] = match;
+    let host = match[2];
+    if (!host) {
+        throw new TypeError(`"${pattern}" does not have a valid host`);
+    }
+
+    let regex = "^";
+
+    if (scheme === "*") {
+        regex += "(http|https)";
+    } else {
+        regex += scheme;
+    }
+
+    regex += "://";
+
+    if (host && host === "*") {
+        regex += "[^/]+?";
+    } else if (host) {
+        if (host.match(/^\*\./)) {
+            regex += "[^/]*?";
+            host = host.substring(2);
+        }
+        regex += host.replace(regexpChars, "\\$&");
+    }
+
+    if (path) {
+        if (path === "*") {
+            regex += "(/.*)?";
+        } else if (path.charAt(0) !== "/") {
+            regex += "/";
+            regex += path.replace(regexpChars, "\\$&").replace(/\*/g, ".*?");
+            regex += "/?";
+        }
+    }
+
+    regex += "$";
+    return new RegExp(regex);
+}
+
 function isValidRegExp(pattern) {
     try {
         new RegExp(pattern);
